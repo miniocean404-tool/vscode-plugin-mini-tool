d9e2c81a6656f8974093e0a5d14c7b9547dd4b7f
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-05-30 17:55:48 +0800

    💡 bulb: 添加注释

--- src/css-px-ignore/index.ts
+++ src/css-px-ignore/index.ts
@@ -2,11 +2,24 @@ import * as vscode from "vscode"
 
 export function addCssPxIgnoreCommand(): vscode.Disposable {
   const disposable = vscode.commands.registerCommand("mini-tool.addCssPxIgnore", () => {
+    // 获取当前活动的编辑器
     const editor = vscode.window.activeTextEditor
 
     if (editor) {
       editor.edit((editBuilder) => {
         const text = editor.document.getText()
+
+        // 获取选中文本的结束位置
+        // const selectionEnd = editor.selection.end
+
+        // 计算下一行的位置
+        // const nextLine = new vscode.Position(selectionEnd.line + 1, 0)
+
+        // 插入新行并输出文本
+        // editor.edit(editBuilder => {
+        // 	editBuilder.insert(nextLine, selectedText + '\n');
+        // });
+
         vscode.window.showInformationMessage(text)
       })
     }
