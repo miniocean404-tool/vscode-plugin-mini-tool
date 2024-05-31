790acb7292afdc7a8740ff5d929f2d0feb260b84
MiniOcean404 <1037306928@qq.com>
2024-05-31 22:27:24 +0800

    🎨 art: 文件结构

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode"
-import { addRegionToSelection } from "./region"
-import { addCssPxIgnoreCommand } from "./css-px-ignore"
+import { addRegionToSelection } from "./command/region"
+import { addCssPxIgnoreCommand } from "./command/css-px-ignore"
 
 // command id 参数必须与 package.json 中 的 command 字段匹配
 export function activate(context: vscode.ExtensionContext) {
