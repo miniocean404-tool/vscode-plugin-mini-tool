9a5cdaf33eaeef3d3681f9b0c89bf1ffd02bb3ee
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-06-06 14:55:02 +0800

    🔖 bookmark: 更新版本

--- index.js
+++
@@ -1,5 +0,0 @@
-const handleRemoveRepeat = (arr) => arr.filter((item, index) => arr.indexOf(item, 0) === index)
-
-const a = [{ a: "height" }, { b: "weight" }, { c: "www" }, { a: "height" }]
-
-console.log(handleRemoveRepeat(a))
