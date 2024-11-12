6159278450633c3eb9a4b0cb6c19b6f78fda790e
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-11-12 10:55:17 +0800

    ✨ feat: 删除无用文件

--- src/utils/uri.ts
+++
@@ -1,5 +0,0 @@
-import * as vscode from "vscode"
-import path from "path"
-import { homedir } from "os"
-
-export const getDesktopFileURI = (filepath: string) => vscode.Uri.file(path.resolve(homedir(), "Desktop", filepath))
