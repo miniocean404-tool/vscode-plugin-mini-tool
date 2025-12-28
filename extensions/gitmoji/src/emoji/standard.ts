6984bfed8354228af61a2d9cc936292bfbdd2939
MiniOcean404 <1037306928@qq.com>
2025-12-29 02:13:58 +0800

    ♻️ refactor: 处理大小写

--- extensions/gitmoji/src/emoji/standard.ts
+++ extensions/gitmoji/src/emoji/standard.ts
@@ -3,7 +3,7 @@ import * as vscode from "vscode"
 export const StandardEmoji = [
   { placeholder: "feat:", description: "新功能", emoji: "✨", code: ":sparkles:" },
   { placeholder: "fix:", description: "修复", emoji: "🐛", code: ":bug:" },
-  { placeholder: "ai:", description: "AI 相关变更", emoji: "🤖", code: ":robot:" },
+  { placeholder: "ai:", description: "ai 相关变更", emoji: "🤖", code: ":robot:" },
   { placeholder: "docs:", description: "文档变更", emoji: "📝", code: ":memo:" },
   { placeholder: "style:", description: "代码格式 (不影响代码运行的变动)", emoji: "💄", code: ":lipstick:" },
   {
@@ -15,7 +15,7 @@ export const StandardEmoji = [
   { placeholder: "perf:", description: "性能优化", emoji: "⚡️", code: ":zap:" },
   { placeholder: "test:", description: "增加测试", emoji: "✅", code: ":white_check_mark:" },
   { placeholder: "build:", description: "打包", emoji: "📦", code: ":package:" },
-  { placeholder: "ci:", description: "对 CI 配置文件修改", emoji: "🎡", code: ":ferris_wheel:" },
+  { placeholder: "ci:", description: "对 ci 配置文件修改", emoji: "🎡", code: ":ferris_wheel:" },
   { placeholder: "chore:", description: "构建过程或辅助工具的变动", emoji: "🔨", code: ":hammer:" },
   { placeholder: "revert:", description: "回退", emoji: "⏪", code: ":rewind:" },
   {
